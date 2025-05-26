c0401e574f1a8b024c99af05ccf84a9587088445
Anton Poroshkin <anton.poroshkin@nure.ua>
2025-05-26 22:41:10 +0300

    chore: remove playwright

--- playwright/index.tsx
+++
@@ -1,7 +0,0 @@
-// Import styles, initialize component theme here.
-// import '../src/common.css';
-
-import "@/assets/sass/common/base.scss"
-import "@/assets/css/light.css"
-
-import "swiper/swiper-bundle.css"
